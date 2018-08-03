5296eb775cef02fed86dcfd85fabca4ba307153c
peter279k <peter279k@gmail.com>
2018-08-03 17:58:26 +0800

    Test enhancement

--- tests/TypedArrayTest.php
+++ tests/TypedArrayTest.php
@@ -9,6 +9,8 @@
  */
 declare(strict_types=1);
 
+namespace Linna\Tests;
+
 use Linna\TypedArray;
 use PHPUnit\Framework\TestCase;
 
@@ -283,8 +285,6 @@ class TypedArrayTest extends TestCase
         $arrayAsParam = ['a','b','c','d','e','f','g','h','i'];
         $array = new TypedArray('string', $arrayAsParam);
 
-        foreach ($array as $key => $value) {
-            $this->assertEquals($value, $arrayAsParam[$key]);
-        }
+        $this->assertEquals($arrayAsParam, iterator_to_array($array));
     }
 }
